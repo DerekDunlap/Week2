e565417a783cc01cecc5eee995d8fdd787672f74
Derek Dunlap <derekdunlapjr@gmail.com>
2021-07-13 22:13:32 -0400

    added delete features for shopping_lists & grocery_list

--- grocery_app.py
+++ grocery_app.py
@@ -16,15 +16,12 @@ class ShoppingList:
         #add grocery item to a particular grocery list/store shopping list
         #Add item(s) to your Shopping List
         title=input("\nPlease enter the name of the item or Press 'q' to go back to Main Menu: ")
-        if(title!='q' or title!='Q'):
-            price=float(input("Please enter the price of the item: $"))
-            quantity=int(input("Please enter the quantity that you wish to purchase: "))
-            item=GroceryItem(title,price,quantity)
-            self.grocery_items.append(item)
-            os.system("clear")
-        else:
-            os.system("clear")
-            return None
+        price=float(input("Please enter the price of the item: $"))
+        quantity=int(input("Please enter the quantity that you wish to purchase: "))
+        item=GroceryItem(title,price,quantity)
+        shopping_list.grocery_items.append(item)
+        os.system("clear")
+        
 
     def display_shopping_lists(self):
         os.system("clear")
@@ -54,7 +51,7 @@ class GroceryItem:
 #Ask for user input
 
 while(True):
-    choice=input("Press 1 to Create a Shopping List: \n\nPress 2 to View All Shopping Lists: \n\nPress 3 to View One Shopping List: \n\nPress q to Quit the Program: \n\nEnter choice: ")
+    choice=input("Press 1 - Add a store to Shopping List:\nPress 2 - Delete store from Shopping List:\nPress 3 - View all stores on Shopping List:\nPress 4 - View a particular store's Grocery List:\nPress Q - Quit Program:\n\nEnter choice: ")
     if(choice=='1'):
         #Add a store to the Shopping List
         os.system("clear")
@@ -67,22 +64,29 @@ while(True):
         shopping_list.add_grocery_item()
             
     elif(choice=='2'):
-        #View all shopping lists
+        #Delete from shopping_lists
+        os.system("clear")
+        shopping_list.display_shopping_lists()
+        number=int(input("Enter the number of the store that you wish to delete: "))
+        del shopping_lists[number-1]
         shopping_list.display_shopping_lists()
 
     elif(choice=='3'):
+        #View all shopping lists
+        shopping_list.display_shopping_lists()
+
+    elif(choice=='4'):
         #Displays all shopping lists to allow user to input desired store's shopping list
         shopping_list.display_shopping_lists()
         number=int(input("Please enter the number of the Store that you wish to view: "))
         #View shopping list for a particular start (accepts number as a argument)
         while True:
             shopping_list.display_shopping_list(number)
-            option=input("\n\nEnter 'Y'- Add an item to this shopping list: \nEnter 'N'- To return to the Main Menu:\n\nEnter option: ")
-            if(option=='Y' or option=='y'):
+            option=input("\n\nEnter 'A'- Add an item to this shopping list: \nEnter 'D'- To return to the Main Menu:\n\nEnter option: ")
+            if(option=='A' or option=='a'):
                 #Provides user witht the option to add a new item to a store's shopping list
                 shopping_list.add_grocery_item()
-                shopping_list.display_shopping_list(number)
-            elif(option=='N' or option=='n'):
+            elif(option=='D' or option=='d'):
                 os.system("clear")
                 break
 
